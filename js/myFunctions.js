b2e63921133ce637b69fb5c8ae9c745d30db9143
nicopskc <90817565+nicopskc@users.noreply.github.com>
2022-05-11 07:12:27 -0300

    updare 2

--- js/myFunctions.js
+++ js/myFunctions.js
@@ -1,28 +1,10 @@
+
+
 function myFunction(imgs) {
-  // Get the expanded image
   var expandImg = document.getElementById("expandedImg");
-  // Get the image text
   var imgText = document.getElementById("imgtext");
-  // Use the same src in the expanded image as the image being clicked on from the grid
   expandImg.src = imgs.src;
-  // Use the value of the alt attribute of the clickable image as text inside the expanded image
   imgText.innerHTML = imgs.alt;
-  // Show the container element (hidden with CSS)
   expandImg.parentElement.style.display = "block";
 }
 
-
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1} 
-  x[slideIndex-1].style.display = "block"; 
-  setTimeout(carousel, 3500); // Change image every 2 seconds
-}
