50fdfb3a4e3f6535994695fed3b5e0138fae6b39
nicopskc <90817565+nicopskc@users.noreply.github.com>
2022-05-14 18:28:42 -0300

    video updates

---
+++ js/video.js
@@ -0,0 +1,14 @@
+
+
+var theVideo = $(".theVideo");
+
+$theVideo.on('mouseover',function() {  
+    $theVideo.get(0).play(); 
+}
+
+$theVideo.on('mouseout',function() 
+    $theVideo.get(0).pause(); 
+}
+
+
+
--- js/zoomIt.js
+++ js/zoomIt.js
@@ -12,11 +12,4 @@ function zoomIt(imgs) {
 }
 
 
-function myFunction(imgs) {
-  var expandImg = document.getElementById("expandedImg");
-  var imgText = document.getElementById("imgtext");
-  expandImg.src = imgs.src;
-  imgText.innerHTML = imgs.alt;
-  expandImg.parentElement.style.display = "block";
-}
 
